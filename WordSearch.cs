0ff692a83bda4eca4cfda58a35a902e8200281c7
cihlar <martin.cihlar@ysoft.com>
2022-05-01 12:24:45 +0200

    in the middle of changeing implemetation
    this approach just tries all direction instead of calculation possible viable directions

--- WordSearch.cs
+++ WordSearch.cs
@@ -8,6 +8,35 @@ public class WordSearch
     private readonly int gridWidth;
     private readonly int gridHeight;
 
+    private class WordLocator
+    {
+        private readonly string word;
+        private readonly (int, int) wordStart;
+        public (int, int)? wordEnd;
+        public bool isWordFound = false;
+        
+        public WordLocator(string word, (int, int) wordStart)
+        {
+            this.word = word;
+            this.wordStart = wordStart;
+        }
+
+        public (int, int)? FindEndOfWordInGrid()
+        {
+            CheckRight();
+            CheckLeft();
+            CheckUp();
+            CheckDown();
+            CheckDiagLeftUp();
+            CheckDiagRightUp();
+            CheckDiagRightDown();
+            CheckDiagLeftDown();
+            return wordEnd;
+        }
+        
+
+    }
+
     public WordSearch(string grid)
     {
         gridHeight = grid.Split("\n").Length;
@@ -36,53 +65,6 @@ public class WordSearch
 
         return results;
     }
-    
-    private int? GetDirection(int wordLength, (int startX, int startY) start, (int endX, int endY) end)
-    {
-        var rangeX = end.endX - start.startX + 1;
-        var rangeY = end.endY - start.startY + 1;
-        var doesRangeNotEqualWordLength = wordLength != Math.Abs(rangeX) && wordLength != Math.Abs(rangeY);
-        if (doesRangeNotEqualWordLength)
-        {
-            return null;
-        }
-
-        return (rangeX, rangeY) switch
-        {
-            (1, var y) when y == wordLength => 1,
-            var (x, y) when x == wordLength && y == wordLength => 2,
-            (var x, 1) when x == wordLength => 3,
-            var (x, y) when x == wordLength && y == -wordLength => 4,
-            (1, var y) when y == -wordLength => 5,
-            var (x, y) when x == -wordLength && y == -wordLength => 6,
-            (var x, 1) when x == -wordLength => 7,
-            var (x, y) when x == -wordLength && y == wordLength => 8,
-            _ => null
-        };
-
-    }
-
-    private ((int, int), (int, int))? FindWordInGrid(string word, List<(int, int)> startCoordinates, List<(int, int)> endCoordinates)
-
-    {
-        foreach (var start in startCoordinates)
-        {
-            foreach (var end in endCoordinates)
-            {
-                // int? result = GetDirection(word.Length, start, end) switch
-                // {
-                //     _ => null
-                // };
-                if (GetDirection(word.Length, start, end) != null)
-                {
-                    return (start, end);
-                }
-            }
-        }
-
-        return null;
-    }
-
 
     public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor)
     {
@@ -90,21 +72,27 @@ public class WordSearch
         foreach (var word in wordsToSearchFor)
         {
             var startCoordinates = FindLetterInGrid(word.First());
-            var endCoordinates = FindLetterInGrid(word.Last());
-            if(!startCoordinates.Any() | !endCoordinates.Any())
+            foreach (var wordStart in startCoordinates)
             {
-                results.Add(word, null); 
-                continue;
+                var wordLocator = new WordLocator(word, wordStart);
+                wordLocator.FindEndOfWordInGrid();
+                if (wordLocator.isWordFound)
+                {
+                    results.Add(word, ToHumanReadable((wordStart, wordLocator.wordEnd)));
+                    break;
+                }
             }
 
-            var result = FindWordInGrid(word, startCoordinates, endCoordinates);
-            results.Add(word, ToHumanReadable(result));
+            if (!results.ContainsKey(word))
+            {
+                results.Add(word, null);
+            }
             
         }
 
         return results;
     }
 
-    private ((int, int), (int, int))? ToHumanReadable(((int, int), (int, int))? result) 
-        => result == null ? null : ((result.Value.Item1.Item2 + 1, result.Value.Item1.Item1 + 1), (result.Value.Item2.Item2 + 1, result.Value.Item2.Item1 + 1));
+    private ((int, int), (int, int))? ToHumanReadable(((int, int), (int, int)?) result) 
+        => result.Item2 == null ? null : ((result.Item1.Item2 + 1, result.Item1.Item1 + 1), (result.Item2.Value.Item2 + 1, result.Item2.Value.Item1 + 1));
 }
